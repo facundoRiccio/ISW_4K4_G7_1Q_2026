84b6d3c5a51e0ee059473f4f4437662591211f4c
Joaquin Bonugli <joaquin.bonugli@mi.unc.edu.ar>
2026-05-25 20:38:32 -0300

    feat: (green) implementación validación tipo y rango de cantidad de entradas

--- trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/cantidad.js
+++ trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/cantidad.js
@@ -1,3 +1,6 @@
 export const validarCompraEntradas = (number) => {
-    
+    if (Number.isNaN(number)) throw new Error('El parámetro debe ser un número');
+    if (number > 10) throw new Error('El número de entradas no puede ser mayor a 10');
+    if (number < 1) throw new Error('El número de entradas no puede ser menor a 1');
+    return true;
 }
