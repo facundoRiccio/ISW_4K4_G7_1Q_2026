a611990834b0d3a0bfd0df212d93b7875af68d1a
Joaquin Bonugli <joaquin.bonugli@mi.unc.edu.ar>
2026-05-25 23:19:53 -0300

    add: (green) implementando formas de pago válidas

--- trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/formapago.js
+++ trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/formapago.js
@@ -1,3 +1,13 @@
 export const validarFormaPago = (formaPago) => {
-  
+  if (!formaPago || typeof formaPago !== 'string') {
+    throw new Error('Debe seleccionar una forma de pago')
+  }
+
+  const opcionesPermitidas = ['efectivo', 'tarjeta']
+
+  if (!opcionesPermitidas.includes(formaPago.toLowerCase())) {
+    throw new Error('La forma de pago debe ser "tarjeta" o "efectivo"')
+  }
+
+  return true
 }
