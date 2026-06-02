35cba730019d5dff7fc53891bfe50a1eb0b5ee7d
gregoriotonelli <gregoriotone100@gmail.com>
2026-06-02 18:42:44 -0300

    controlar estilos

--- trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/cantidad.js
+++ trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/src/cantidad.js
@@ -1,7 +1,7 @@
 export const validarCompraEntradas = (cantidad) => {
-    if (Number.isNaN(cantidad)) throw new Error('El parámetro debe ser un número');
-    if (!Number.isInteger(cantidad)) throw new Error('El número de entradas debe ser un número entero');
-    if (cantidad > 10) throw new Error('El número de entradas no puede ser mayor a 10');
-    if (cantidad < 1) throw new Error('El número de entradas no puede ser menor a 1');
-    return true;
+  if (Number.isNaN(cantidad)) throw new Error('El parámetro debe ser un número')
+  if (!Number.isInteger(cantidad)) throw new Error('El número de entradas debe ser un número entero')
+  if (cantidad > 10) throw new Error('El número de entradas no puede ser mayor a 10')
+  if (cantidad < 1) throw new Error('El número de entradas no puede ser menor a 1')
+  return true
 }
--- trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/tests/cantidad.test.js
+++ trabajos_practicos_evaluables/trabajo_evaluable_6/entrega-tp-6/backend/tests/cantidad.test.js
@@ -1,25 +1,24 @@
-import { describe, it, expect } from 'vitest';
-import { validarCompraEntradas } from '../src/cantidad.js';
+import { describe, it, expect } from 'vitest'
+import { validarCompraEntradas } from '../src/cantidad.js'
 
 describe('validarCompraEntradas', () => {
   it('debería retornar un error si el número de entradas no es un número', () => {
-    expect(() => validarCompraEntradas(NaN)).toThrow('El parámetro debe ser un número');
+    expect(() => validarCompraEntradas(NaN)).toThrow('El parámetro debe ser un número')
   })
 
   it('debería lanzar un error si el número de entradas no es un entero', () => {
-    expect(() => validarCompraEntradas(2.5)).toThrow('El número de entradas debe ser un número entero');
+    expect(() => validarCompraEntradas(2.5)).toThrow('El número de entradas debe ser un número entero')
   })
 
   it('debería lanzar un error si el número de entradas es mayor a 10', () => {
-    expect(() => validarCompraEntradas(21)).toThrow('El número de entradas no puede ser mayor a 10');
+    expect(() => validarCompraEntradas(21)).toThrow('El número de entradas no puede ser mayor a 10')
   })
 
   it('debería lanzar un error si el número de entradas es menor a 1', () => {
-    expect(() => validarCompraEntradas(0)).toThrow('El número de entradas no puede ser menor a 1');
+    expect(() => validarCompraEntradas(0)).toThrow('El número de entradas no puede ser menor a 1')
   })
 
   it('debería devolver true si el número de entradas está entre 1 y 10', () => {
-    expect(validarCompraEntradas(10)).toBe(true);
-  });
-
-});
+    expect(validarCompraEntradas(10)).toBe(true)
+  })
+})
